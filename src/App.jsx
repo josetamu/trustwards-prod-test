b421fa6e0ec3bf96f0d4f1c0f59fffed0a313f91
Oscar Abad <oscar.abad.brickscore@gmail.com>
2025-05-20 11:50:36 +0200

    working sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,24 @@ import './App.css'
 function App() {
   const [cards, setCards] = useState([]);
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const pages = ['Domains', 'Settings'];
+  const pages = [
+    {
+      name: 'Domains',
+      icon: <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" color="#ffffff" fill="none">
+      <path d="M2.5 12C2.5 7.52166 2.5 5.28249 3.89124 3.89124C5.28249 2.5 7.52166 2.5 12 2.5C16.4783 2.5 18.7175 2.5 20.1088 3.89124C21.5 5.28249 21.5 7.52166 21.5 12C21.5 16.4783 21.5 18.7175 20.1088 20.1088C18.7175 21.5 16.4783 21.5 12 21.5C7.52166 21.5 5.28249 21.5 3.89124 20.1088C2.5 18.7175 2.5 16.4783 2.5 12Z" stroke="#ffffff" stroke-width="1.5"></path>
+      <path d="M2.5 9H21.5" stroke="#ffffff" stroke-width="1.5" stroke-linejoin="round"></path>
+      <path d="M7 6H7.00898" stroke="#ffffff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+      <path d="M11 6H11.009" stroke="#ffffff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+      <path d="M9.11805 13.5C8.84339 13.1931 8.44425 13 8 13C7.17157 13 6.5 13.6716 6.5 14.5V15.5C6.5 16.3284 7.17157 17 8 17C8.44425 17 8.84339 16.8069 9.11805 16.5" stroke="#ffffff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+      <path d="M15.5 13L17.5 17" stroke="#ffffff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+      <path d="M12.4922 13.5H12.5012M12.4922 16.5H12.5012" stroke="#ffffff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+  </svg>
+    },
+    {
+      name: 'Settings',
+      icon: settingsIcon
+    }
+  ];
 
   // creates a custom new card
   const addCard = (text) => {
@@ -16,6 +33,9 @@ function App() {
   };
 
   return (
+    <div className="app-container">
+      <Sidebar pages={pages} />
+    
     <div className="card-table">
       <button onClick={() => setIsModalOpen(true)} className="md-card-button">
         Add Domain
@@ -32,6 +52,7 @@ function App() {
           onSave={addCard}
         />
       )}
+    </div>
     </div>
   );
 }
