0950f364f9ff52c964cb79b42e99f1ecd37d28ea
Oscar Abad <oscar.abad.brickscore@gmail.com>
2025-05-27 15:04:59 +0200

    sidebar done and user config started

--- src/dashboard/profileDropdown/ProfileDropdown.jsx
+++ src/dashboard/profileDropdown/ProfileDropdown.jsx
@@ -35,19 +35,19 @@ export const ProfileDropdown = () => {
     return (
         <div className="profileDropdown">
             <div className="profileDropdown__dropdown">
-                        <div className="sidebar__profile-upper">
+                        <div className="profileDropdown__upper">
                             {profilePages.map((profilePage) => (
                                 <SidebarLink
-                                    className="sidebar__profile-link"
+                                    className="profileDropdown__link"
                                     key={profilePage.name}
                                     icon={profilePage.icon}
                                     text={profilePage.name}
                                 />
                             ))}
                         </div>
-                        <div className="sidebar__profile-lower">
+                        <div className="profileDropdown__lower">
                             <SidebarLink
-                                className="sidebar__profile-link logout"
+                                className="profileDropdown__link profileDropdown__link--logout"
                                 icon={<svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
                                     <g clip-path="url(#clip0_64_417)">
                                     <path d="M12.541 7.00067C12.541 4.38829 12.541 3.0821 11.7295 2.27054C10.9179 1.45898 9.61171 1.45898 6.99935 1.45898C4.38701 1.45898 3.08081 1.45898 2.26921 2.27054C1.45768 3.0821 1.45768 4.38829 1.45768 7.00067C1.45768 9.61301 1.45768 10.9192 2.26921 11.7307C3.08081 12.5423 4.38701 12.5423 6.99935 12.5423C9.61171 12.5423 10.9179 12.5423 11.7295 11.7307C12.541 10.9192 12.541 9.61301 12.541 7.00067Z" stroke="#686B74" stroke-width="1.3"/>
@@ -66,16 +66,16 @@ export const ProfileDropdown = () => {
             </div>
             <div className="profileDropdown__profile">
             <div className="profileDropdown__header">
-                <img className="profileDropdown__header-avatar" src="https://cdn-icons-png.flaticon.com/512/1308/1308845.png" alt="avatar" />
-                <span className="profileDropdown__header-name">{user?.user_metadata?.display_name || 'User'}</span>
+                <img className="profileDropdown__header--avatar" src="https://cdn-icons-png.flaticon.com/512/1308/1308845.png" alt="avatar" />
+                <span className="profileDropdown__header--name">{user?.user_metadata?.display_name || 'User'}</span>
             </div>
             <div className="profileDropdown__icons">
-                <span className="profileDropdown__icons-down">
+                <span className="profileDropdown__icons--down">
                     <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M3.00002 4.5C3.00002 4.5 5.20948 7.49999 6.00003 7.5C6.79058 7.5 9 4.5 9 4.5" stroke="#6B6B6B" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
                     </svg>
                 </span>
-                <span className="profileDropdown__icons-up">
+                <span className="profileDropdown__icons--up">
                     <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M3.00002 7.5C3.00002 7.5 5.20948 4.50001 6.00003 4.5C6.79058 4.5 9 7.5 9 7.5" stroke="#6B6B6B" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
                     </svg>
--- src/dashboard/settings/Settings.jsx
+++ src/dashboard/settings/Settings.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { supabase } from '../supabase/supabaseClient';
+import { supabase } from '../../supabase/supabaseClient';
 
 
 export function Settings({ onClose }) {
