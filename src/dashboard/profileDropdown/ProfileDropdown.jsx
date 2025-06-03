0aeadeb203f9acf01552a411ed3ccfa2a19fa05a
Oscar Abad <oscar.abad.brickscore@gmail.com>
2025-06-03 13:00:29 +0200

    profileDropdown improvements

--- src/dashboard/profileDropdown/ProfileDropdown.jsx
+++ src/dashboard/profileDropdown/ProfileDropdown.jsx
@@ -39,9 +39,6 @@ export const ProfileDropdown = ({ setUserSettings,setIsSidebarOpen,user,isDropdo
     
     return (
         <div className={`${isSidebarOpen ? 'profileDropdown--open' : 'profileDropdown'}`}>
-            <div className="felipevi">
-                
-            </div>
             <div className=
                     {isMobile 
                         ? `${isDropdownOpen ? "profileDropdown__dropdown--active" : "profileDropdown__dropdown--inactive"}`
@@ -85,7 +82,7 @@ export const ProfileDropdown = ({ setUserSettings,setIsSidebarOpen,user,isDropdo
                             />
                         </div>
             </div>
-
+            
            <div className={`${isSidebarOpen ? 'profileDropdown__profile--open' : 'profileDropdown__profile'}`} onClick={() => {
               toggleDropdown();
             }}>
