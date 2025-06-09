1ba59b82fa23ada6fcd6fbaf426e972428686a55
Oscar Abad <oscar.abad.brickscore@gmail.com>
2025-06-09 10:48:33 +0200

    commit

--- src/dashboard/sidebarSites/SidebarSites.jsx
+++ src/dashboard/sidebarSites/SidebarSites.jsx
@@ -1,12 +1,10 @@
 import "./SidebarSites.css";
 
-
-
 export function SidebarSites ({avatar, name}) {
     return(
         <div className="sidebarSites__site">
             <span className="sidebarSites__site-avatar">
-                <img src='https://www.freepik.com/free-vector/mysterious-mafia-man-smoking-cigarette_7074311.htm#fromView=keyword&page=1&position=14&uuid=38c2c6df-e810-4ebb-b321-7f6e28a55dfd&query=Avatar' alt={avatar} />
+                <img src={avatar} alt={name} />
             </span>
             <span className="sidebarSites__site-name">
                 {name}
