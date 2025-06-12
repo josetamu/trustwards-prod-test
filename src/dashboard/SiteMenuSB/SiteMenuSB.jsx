2ba212d4c0ee6282667126b2c8d160492c1bf7c2
Oscar Abad <oscar.abad.brickscore@gmail.com>
2025-06-12 10:46:33 +0200

    sidebar sites overflows adjusted, modal mobile breakpoint adjusted, sidebar mobile improvements

--- src/dashboard/SiteMenuSB/SiteMenuSB.jsx
+++ src/dashboard/SiteMenuSB/SiteMenuSB.jsx
@@ -15,7 +15,7 @@ const SiteMenuTrigger = () => {
     return(
     <div className="sidebarSites__edit">
         <svg width="9" height="2" viewBox="0 0 9 2" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M4.49563 1H4.50437M1 1H1.00874M7.99126 1H8" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M4.49563 1H4.50437M1 1H1.00874M7.99126 1H8" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
         </svg>
     </div>
 )
--- src/dashboard/sidebarSite/SidebarSites.jsx
+++ src/dashboard/sidebarSite/SidebarSites.jsx
@@ -1,11 +1,11 @@
 import "./SidebarSites.css";
 import { useId } from "react";
 import { SiteMenuSB } from "../SiteMenuSB/SiteMenuSB";
-export function SidebarSites ({avatar, name}) {
+export function SidebarSites ({avatar, name, isSidebarOpen}) {
     const sidebarSitesId = useId();
     
     return(
-        <div className="sidebarSites__site" id={sidebarSitesId}>
+        <div className={`${isSidebarOpen ? 'sidebarSites__site--open' : 'sidebarSites__site'}`} id={sidebarSitesId}>
             <div className="sidebarSites__header">
             <span className="sidebarSites__header-avatar">
                 <img className="sidebarSites__header-avatar-img" src={avatar}/>
