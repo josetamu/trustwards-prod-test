4ee68852ccac2729584f9b9a52f052913497b8a4
josetamu <jose11tamu@gmail.com>
2025-06-27 11:18:26 +0200

    Added dark mode

--- src/dashboard/App.jsx
+++ src/dashboard/App.jsx
@@ -80,7 +80,7 @@ function App() {
   const _loginDevUser = async () => {
     await supabase.auth.signInWithPassword({
       /* emails: 'darezo.2809@gmail.com', 'oscar.abad.brickscore@gmail.com', 'jose11tamu@gmail.com'*/
-      email: 'oscar.abad.brickscore@gmail.com',  
+      email: 'darezo.2809@gmail.com',  
       password: 'TW.141109'
     });
   };
