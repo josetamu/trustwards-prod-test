038f31270eae5eca8656a5f2e472329f4c37d1f9
Oscar Abad <oscar.abad.brickscore@gmail.com>
2025-07-17 08:22:28 +0200

    trying to fixed sidebar flickering

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,9 +1,29 @@
 import { ThemeProvider } from 'next-themes';
+import { supabase } from '../supabase/supabaseClient';
 
-export default function RootLayout({ children }) {
+export default async function RootLayout({ children }) {
     /*  Note! If you do not add suppressHydrationWarning to your <html> you will get warnings because next-themes updates that element. 
         This property only applies one level deep, so it won't block hydration warnings on other elements. 
     */
+   //Force login (only dev mode)
+  const _loginDevUser = async () => {
+    await supabase.auth.signInWithPassword({
+      /* emails: 'darezo.2809@gmail.com', 'oscar.abad.brickscore@gmail.com', 'jose11tamu@gmail.com'*/
+      email: 'oscar.abad.brickscore@gmail.com',  
+      password: 'TW.141109'
+    });
+  };
+  await _loginDevUser();
+   const { data: { user } } = await supabase.auth.getUser();
+   
+   // Only fetch appearance data if user is authenticated
+   let initialData = null;
+   if (user) {
+       initialData = await supabase.from('Appearance').select('*').eq('userid', user.id).single();
+       console.log(initialData);
+       console.log(initialData?.Sidebar);
+   }
+   
     return (
         <html lang="en" suppressHydrationWarning> 
             <head>
@@ -11,7 +31,7 @@ export default function RootLayout({ children }) {
             </head>
             <body>
                 <ThemeProvider> {/* Client component from next-themes */}
-                    <div id="root">{children}</div>
+                    <div data-sidebar={`${initialData?.Sidebar}`} id="root">{children}</div>
                 </ThemeProvider>
             </body>
         </html>
