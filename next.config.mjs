82e63f58db5cca7b4a05650f73d983fe8f9c258a
josetamu <jose11tamu@gmail.com>
2025-10-10 10:53:03 +0200

    fix: remove custom distDir to use Next.js default .next directory

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  distDir: './dist',
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
   devIndicators: false,
   serverExternalPackages: ['playwright'],
